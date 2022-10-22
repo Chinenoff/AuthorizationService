7af53e9c9db671636fae279a132244c4fbf54b63
ChinenOFF <pro100cvv@gmail.com>
2022-10-22 15:32:36 +0300

    fix user creation

--- src/main/java/com/example/authorizationservice/repository/UserRepository.java
+++ src/main/java/com/example/authorizationservice/repository/UserRepository.java
@@ -13,13 +13,14 @@ import java.util.concurrent.ConcurrentHashMap;
 public class UserRepository {
     private final static Map<String, User> userMap = new ConcurrentHashMap<>();
 
-    public List<Authorities> getUserAuthorities(String user, String password) {
-
-        //test block
+    public UserRepository() {
+        //test users
         userMap.put("john", new User("john", "qwerty", Arrays.asList(Authorities.READ, Authorities.WRITE)));
         userMap.put("neo", new User("neo", "matrix13", Arrays.asList(Authorities.READ, Authorities.WRITE, Authorities.DELETE)));
-        //end test block
+        //end test users
+    }
 
+    public List<Authorities> getUserAuthorities(String user, String password) {
         List<Authorities> foundUserAuthoities = null;
         if (userMap.containsKey(user) && userMap.get(user).getPassword().equals(password)) {
             foundUserAuthoities = userMap.get(user).getAuthoritiesList();
